7b026bbfe2fe53c5d6aee455c6705ef87b9162b4
Ditt07 <stevsr0705@gmail.com>
2025-04-24 01:37:55 +0200

    Hell yeah

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
+from stats import get_num_words
+import sys
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1] 
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
     print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
     print()
 
     for item in chars_sorted_list:
         if not item["char"].isalpha():
             continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
+        print(f"{item['char']}: {item['num']}")
 
     print("--- End report ---")
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
 
 def sort_on(d):
     return d["num"]
